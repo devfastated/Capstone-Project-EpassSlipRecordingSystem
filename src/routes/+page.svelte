9680792f105aaf52cedad0760c891824eb7829dd
devfastated <businessmailromer@gmail.com>
2024-03-13 02:42:25 +0800

    toggle

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,48 +1,51 @@
 <script>
-   let message = '...';
-   let imageVisible = false;
-   
-    function showMessage(){
-        message += "    Iloveyou";
+	let imageVisible = false
+	let msgVisible = false
+	let message = ''
+	let image
 
-    }
+	function showMessage() {
+		if (msgVisible) {
+			message = ''
+			msgVisible = false
+		} else {
+			message = 'Iloveyou'
+			msgVisible = true
+		}
+		console.log(message)
+	}
 
-    function toggleImage(){
-      let  image = document.getElementById('image'); 
-      image.src = '/love.png';
-      if (imageVisible){
-        image.style.display = 'none';
-        imageVisible = false;
-      } else {
-        image.style.display = 'block';
-        imageVisible = true;
-      }
-    }
-
+	function toggleImage() {
+		image = document.getElementById('image')
+		image.src = '/love.png'
+		if (imageVisible) {
+			image.style.display = 'none'
+			imageVisible = false
+		} else {
+			image.style.display = 'block'
+			imageVisible = true
+		}
+	}
 </script>
 
+<h1>This is {message}</h1>
 
-
-<h1>My name is  {message}  </h1> 
-
-<button on:click={showMessage}>Click me! I have surprise</button>
-<img id="image" alt="">
+<button on:click={showMessage}
+	>Click me! I have surprise</button
+>
+<img id="image" alt="" />
 <button on:click={toggleImage}>Show Image</button>
 
 <style>
-    h1{
-        color: goldenrod;
-        font-family: 'Comic Sans MS', cursive;
-        font-size: 2em;
-    }
+	h1 {
+		color: goldenrod;
+		font-family: 'Comic Sans MS', cursive;
+		font-size: 2em;
+	}
 
-    button {
-        border: 1px solid black;
-        padding: 2px ;
-        border-radius: 5px;
-        
-        
-    }
+	button {
+		border: 1px solid black;
+		padding: 2px;
+		border-radius: 5px;
+	}
 </style>
-
-
