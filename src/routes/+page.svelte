99166a84f683439b0bf80a6660feb0445603587d
devfastated <businessmailromer@gmail.com>
2024-03-12 17:56:58 +0800

    second commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+
+
+    let count = 0;
+    function increment(){
+       count++;
+       console.log('count', count);
+    }
+</script>
+<div class="ma">
+
+    <button on:click={increment}>Total: {count}</button>
+
+
+</div>
+
+<style>
+    .ma{
+        display: fle;
+        align-items: center;
+        width: 100px;
+    }
+</style>
